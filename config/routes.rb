09498f9abc235eff42ba73b53198a40be89d2a5d
Jonathas Melo <jonathas_ruam@hotmail.com>
2013-07-09 00:32:15 -0300

    Configurando root para home index

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 SelectionManager::Application.routes.draw do
-<<<<<<< HEAD
-  get "home/index"
-=======
+
   resources :candidates
 
->>>>>>> 9f9ac5895735d5437f4fbb03f501276802af98a8
 
   devise_for :users
 
@@ -66,7 +63,7 @@ SelectionManager::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
