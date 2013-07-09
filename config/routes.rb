cd7b5ba133800568b1d0d08cae94d2420ced6199
Jonathas Melo <jonathas_ruam@hotmail.com>
2013-07-09 00:20:55 -0300

    criado controller home,configurado o arquivo routes.rb e excluido public/index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SelectionManager::Application.routes.draw do
+  get "home/index"
+
   devise_for :users
 
   resources :enterprises
@@ -66,4 +68,5 @@ SelectionManager::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  root :to => "home#index"
 end
