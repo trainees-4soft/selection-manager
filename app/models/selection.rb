184cebb8ff15d43cbe78156b008095db65b01416
pontoka <kauepbd@gmail.com>
2013-07-09 21:59:52 -0300

    Adicionar relação Selection belongs_to Enterprise

--- app/models/selection.rb
+++ app/models/selection.rb
@@ -3,4 +3,7 @@ class Selection < ActiveRecord::Base
   has_many :steps, :dependent => :restrict 
   #aqui estou dizendo que seleçao pode ter muitos etapas, 
   #mas que se existir alguma etapa com um seleção, essa seleção nao pode ser deletada(:dependent => :restrict).
+
+  belongs_to :enterprise #Cada seleção pertence a uma empresa.
+
 end
