b4ba74e27a8d729f96203dee45173790958a56c0
Andreza Pollyana <andrezapbl1@gmail.com>
2013-07-11 22:06:55 -0300

    conserto do merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130710003020) do
+ActiveRecord::Schema.define(:version => 20130711042116) do
 
   create_table "candidates", :force => true do |t|
     t.string   "name"
@@ -70,9 +70,9 @@ ActiveRecord::Schema.define(:version => 20130710003020) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.integer  "enterprise_id"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.integer  "enterprise_id"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
