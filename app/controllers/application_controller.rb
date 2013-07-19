66edb2a3350489a5b64c35cd73c4c1649e041b52
Jonathas Melo <jonathas_ruam@hotmail.com>
2013-07-19 03:29:55 -0300

    Corrigido bugs de visualização de steps.Criado menu especifico para cada tipo de usuario logado.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ rescue_from Selection::NoMethodError do |exception|
   flash[:error] = "Access denied."
   redirect_to root_url
 end
+rescue_from ActiveRecord::RecordNotFound do |exception|
+  flash[:error] = "Objeto Inexistente"
+  redirect_to root_url
+end
 end
